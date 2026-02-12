f7c52f796688f1c0de6f99180eb38bcb63b9d702
timbdn01 <timbdn01@gmail.com>
2026-02-12 17:56:21 -0500

    added command handling and login function

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"gator/internal/config"
+)
+
+type state struct {
+	cfg config.Config
+}
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	
+	programState := &state{cfg: cfg}
+
+	cmds := &commands{registeredCommands: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
